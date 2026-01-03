eef607206013c2e6ee4975d1e88a61139c51f272
Ahum Maitra <theahummaitra@gmail.com>
2026-01-03 14:09:07 +0530

    feat : Add Hyprland theming feature and much more, published v1.0.0

--- .config/hypr/scripts/GlobalThemeChanger.sh
+++ .config/hypr/scripts/GlobalThemeChanger.sh
@@ -20,6 +20,7 @@ KITTY_THEME_FOLDER="$THEMES_DIR/$choice/kitty"
 WALLPAPER="$THEMES_DIR/$choice/wallpapers/default.png"
 SOURCE="$KITTY_THEME_FOLDER/colors.conf"
 WAYBAR_WALLUST_COLORS_FILE="$THEMES_DIR/$choice/waybar/wallust/colors-waybar.css"
+WALLUST_HYPRLAND_CONFIG_FILE_THEME="$THEMES_DIR/$choice/hypr/wallust/wallust-hyprland.conf"
 
 if [[ ! -f "$SOURCE" ]]; then
   rofi -e "Theme file not found:\n$SOURCE"
@@ -30,7 +31,9 @@ mkdir -p "$(dirname "$KITTY_CONFIG")"
 
 cp "$SOURCE" "$KITTY_CONFIG"
 
-if [[ -f "$WALLPAPER" ]]; then
+change_wallpaper=$(printf "Change default wallpaper\nYes\nNo" | rofi -dmenu -p "Do you want to change wallpaper?")
+
+if [[ -f "$WALLPAPER" && "$change_wallpaper" == "Yes" ]]; then
   swww img "$WALLPAPER"
   "$SCRIPTSDIR/Refresh.sh"
   sleep 1
@@ -42,4 +45,10 @@ if [[ -f "$WAYBAR_WALLUST_COLORS_FILE" ]]; then
 
 fi
 
+if [[ -f "$WALLUST_HYPRLAND_CONFIG_FILE_THEME" ]]; then
+  mkdir -p "$HOME/.config/hypr/wallust"
+  cp "$WALLUST_HYPRLAND_CONFIG_FILE_THEME" "$HOME/.config/hypr/wallust/wallust-hyprland.conf"
+
+fi
 notify-send -u normal "Theme Changed" "Switched global theme to $choice"
+notify-send -u normal "Press SUPER+ALT+R to Refresh the theme"
